d6b774b70e9504bcbc5e50ad2fe7b807809365ca
byigitt <c2228009@student.cankaya.edu.tr>
2024-11-24 22:55:52 +0300

    refactor(smart-contract-test): update configuration and add scripts

--- smart-contract-test/hardhat.config.js
+++ smart-contract-test/hardhat.config.js
@@ -1,18 +1,41 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     bscTestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
-      // accounts: ['YOUR_PRIVATE_KEY']
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 10000000000
     },
     bscMainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
-      // accounts: ['YOUR_PRIVATE_KEY']
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 5000000000
     }
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    gasPrice: 21,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
   }
 }; 
